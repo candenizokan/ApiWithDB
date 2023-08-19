1ff893ad29d63798593c66d1e7037e6a3836e30e
Okan Candeniz <candenizokan@gmail.com>
2023-08-19 14:06:01 +0300

    add Employee.cs

--- Api.Model/Models/Employee.cs
+++ Api.Model/Models/Employee.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Api.Model.Models
 {
-    internal class Employee
+    public class Employee
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
     }
 }
