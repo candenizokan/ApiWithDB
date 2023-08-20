f36124b0da157212f66e500c5fc34aea78ecfd3c
Okan Candeniz <candenizokan@gmail.com>
2023-08-20 10:28:35 +0300

    using Api.Model.Models;

--- Api.DataAccess/Repositories/Abstract/IEmployeeRepository.cs
+++ Api.DataAccess/Repositories/Abstract/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Api.Model.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,6 @@ namespace Api.DataAccess.Repositories.Abstract
 {
     internal interface IEmployeeRepository
     {
+        Employee
     }
 }
