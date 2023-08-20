a72dfc2a1d204a05c013c67711e1224936727c00
Okan Candeniz <candenizokan@gmail.com>
2023-08-20 11:53:30 +0300

    implement interface

--- Api.Business/Concrete/EmployeeService.cs
+++ Api.Business/Concrete/EmployeeService.cs
@@ -1,11 +1,36 @@
 ﻿using Api.Business.Abstract;
+using Api.Model.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Api.Business.Concrete
 {
-    public class EmployeeService: IEmployeeService
+    public class EmployeeService : IEmployeeService
     {
+        public Employee CreateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee UpdateEmploye(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
