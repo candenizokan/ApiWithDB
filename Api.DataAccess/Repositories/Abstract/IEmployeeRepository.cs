0b63b7bfa6976bb6cbf98290b9a739ee5b0c3229
Okan Candeniz <candenizokan@gmail.com>
2023-08-20 10:30:08 +0300

    Employee GetEmployee(int id);

--- Api.DataAccess/Repositories/Abstract/IEmployeeRepository.cs
+++ Api.DataAccess/Repositories/Abstract/IEmployeeRepository.cs
@@ -8,5 +8,7 @@ namespace Api.DataAccess.Repositories.Abstract
     internal interface IEmployeeRepository
     {
         Employee CreateEmployee(Employee employee);//aldığı employee nesnesini veritabanına ekler ve eklediğini geri döner
+
+        Employee GetEmployee(int id); //aldığı id ye sahip nesneyi geri döner
     }
 }
